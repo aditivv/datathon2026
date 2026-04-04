14c2dc4cb5e0d4d9e46e5c26017539907753db17
Aditi Varia <aditivaria@gmail.com>
2026-04-04 14:07:43 -0700

    created classification model

--- backend/model.py
+++ backend/model.py
@@ -1,1 +1,19 @@
 # we use pytorch to make the model in here
+import torch
+import torch.nn as nn
+
+class JobPredictor(nn.Module):
+    def __init__(self, input_size):
+        super().__init__()
+        self.net = nn.Sequential(
+            nn.Linear(input_size, 256),
+            nn.ReLU(),
+            nn.Dropout(0.3),
+            nn.Linear(256, 128),
+            nn.ReLU(),
+            nn.Dropout(0.3),
+            nn.Linear(128, 3)
+        )
+
+    def forward(self, x):
+        return self.net(x)
